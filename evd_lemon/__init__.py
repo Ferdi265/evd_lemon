f0dfc3439612217af3cea446d3bf55b64e9bff7f
Ferdinand Bachmann <ferdinand.bachmann@yrlf.at>
2023-12-18 21:03:51 +0100

    pyproject.toml: modern python packaging

--- evd_lemon/__init__.py
+++ evd_lemon/__init__.py
@@ -1,11 +1,1 @@
-from . import color
-from . import barmanager
-from . import bar
-
-from . import bluetooth
-from . import mic
-from . import network
-from . import battery
-from . import linux
-
-from . import cli
+from .version import __version__
